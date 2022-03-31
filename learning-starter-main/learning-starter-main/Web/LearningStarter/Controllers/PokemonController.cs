e52ae34aff7b41e037d49005843fba34cf0a84f0
alluna <bbarr5160@gmail.com>
2022-03-31 12:08:00 -0500

    - feat added put and delete functions

--- learning-starter-main/learning-starter-main/Web/LearningStarter/Controllers/PokemonController.cs
+++ learning-starter-main/learning-starter-main/Web/LearningStarter/Controllers/PokemonController.cs
@@ -255,7 +255,7 @@ namespace LearningStarter.Controllers
                 .Items
                 .Any(x => x.Id == pokemon.ItemId);
 
-            if (!isValidAbility)
+            if (!isValidItem)
             {
                 response.AddError("Item", "Invalid item");
             }
@@ -308,6 +308,256 @@ namespace LearningStarter.Controllers
             _dataContext.SaveChanges();
 
             return Created("Pokemon created", response);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Edit(
+            [FromRoute] int id,
+            [FromBody] PokemonUpdateDto pokemon)
+        {
+            var response = new Response();
+
+            var isValidPokemon = _dataContext
+                .Pokemon
+                .Any(x => x.Id == id);
+            
+            if (pokemon == null || !isValidPokemon)
+            {
+                response.AddError("", "Entity cannot be null");
+                return BadRequest(response);
+            }
+            
+            var pokemonSpecies = _dataContext
+                .PokemonSpecies
+                .FirstOrDefault(x => x.Id == pokemon.PokemonSpeciesId);
+
+            if (pokemonSpecies == null)
+            {
+                response.AddError("Pokemon Species", "Pokemon Species not found");
+                return NotFound(response);
+            }
+
+            pokemon.Name = pokemon.Name.Trim();
+            if (string.IsNullOrEmpty(pokemon.Name))
+            {
+                response.AddError("Name","Name cannot be null or empty");
+            }
+
+            if (pokemon.HealthEv > 255)
+            {
+                response.AddError("HealthEv", "HealthEv cannot be greater than 255");
+            }
+            
+            if (pokemon.HealthEv < 0)
+            {
+                response.AddError("HealthEv", "HealthEv cannot be less than 0");
+            }
+            
+            if (pokemon.AttackEv > 255)
+            {
+                response.AddError("AttackEv", "AttackEv cannot be greater than 255");
+            }
+            
+            if (pokemon.AttackEv < 0)
+            {
+                response.AddError("AttackEv", "AttackEv cannot be less than 0");
+            }
+            
+            if (pokemon.DefenseEv > 255)
+            {
+                response.AddError("DefenseEv", "DefenseEv cannot be greater than 255");
+            }
+            
+            if (pokemon.DefenseEv < 0)
+            {
+                response.AddError("DefenseEv", "DefenseEv cannot be less than 0");
+            }
+            
+            if (pokemon.SpecialAttackEv > 255)
+            {
+                response.AddError("SpecialAttackEv", "SpecialAttackEv cannot be greater than 255");
+            }
+            
+            if (pokemon.SpecialAttackEv < 0)
+            {
+                response.AddError("SpecialAttackEv", "SpecialAttackEv cannot be less than 0");
+            }
+            
+            if (pokemon.SpecialDefenseEv > 255)
+            {
+                response.AddError("SpecialDefenseEv", "SpecialDefenseEv cannot be greater than 255");
+            }
+            
+            if (pokemon.SpecialDefenseEv < 0)
+            {
+                response.AddError("SpecialDefenseEv", "SpecialDefenseEv cannot be less than 0");
+            }
+            
+            if (pokemon.SpeedEv > 255)
+            {
+                response.AddError("SpeedEv", "SpeedEv cannot be greater than 255");
+            }
+            
+            if (pokemon.SpeedEv < 0)
+            {
+                response.AddError("SpeedEv", "SpeedEv cannot be less than 0");
+            }
+
+            var evTotal = pokemon.AttackEv + pokemon.HealthEv + pokemon.DefenseEv
+                      + pokemon.SpeedEv + pokemon.SpecialAttackEv + pokemon.SpecialDefenseEv;
+
+            if (evTotal > 510)
+            {
+                response.AddError("Ev Total", "Ev Total cannot be greater than 510");
+            }
+
+            if (pokemon.HealthIv > 31 || pokemon.HealthIv < 0)
+            {
+                response.AddError("Iv", " Iv must be between 0 and 31");
+            }
+            
+            if (pokemon.AttackIv > 31 || pokemon.AttackIv < 0)
+            {
+                response.AddError("Iv", " Iv must be between 0 and 31");
+            }
+
+            if (pokemon.DefenseIv > 31 || pokemon.DefenseIv < 0)
+            {
+                response.AddError("Iv", " Iv must be between 0 and 31");
+            }
+
+            if (pokemon.SpecialAttackIv > 31 || pokemon.SpecialAttackIv < 0)
+            {
+                response.AddError("Iv", " Iv must be between 0 and 31");
+            }
+
+            if (pokemon.SpecialDefenseIv > 31 || pokemon.SpecialDefenseIv < 0)
+            {
+                response.AddError("Iv", " Iv must be between 0 and 31");
+            }
+
+            if (pokemon.SpeedIv > 31 || pokemon.SpeedIv < 0)
+            {
+                response.AddError("Iv", " Iv must be between 0 and 31");
+            }
+
+            var isValidAbility = pokemon.AbilityId == pokemonSpecies.PrimaryAbilityId ||
+                                 pokemon.AbilityId == pokemonSpecies.SecondaryAbilityId ||
+                                 pokemon.AbilityId == pokemonSpecies.HiddenAbilityId;
+
+            if (!isValidAbility)
+            {
+                response.AddError("Ability", "Invalid ability");
+            }
+
+            var isValidItem = _dataContext
+                .Items
+                .Any(x => x.Id == pokemon.ItemId);
+
+            if (!isValidItem)
+            {
+                response.AddError("Item", "Invalid item");
+            }
+
+            if (pokemon.Level > 100 || pokemon.Level < 0)
+            {
+                response.AddError("Level", "Level must be between 0 and 100");
+            }
+
+            if (pokemon.Gender > 2 || pokemon.Gender < 0)
+            {
+                response.AddError("Gender", "Invalid Gender");
+            }
+
+            if (response.HasErrors)
+            {
+                return BadRequest(response);
+            }
+
+            var pokemonToUpdate = _dataContext
+                .Pokemon
+                .First(x => x.Id == id);
+            
+            pokemonToUpdate.Name = pokemon.Name;
+            pokemonToUpdate.PokemonSpeciesId = pokemon.PokemonSpeciesId;
+            pokemonToUpdate.HealthEv = pokemon.HealthEv;
+            pokemonToUpdate.AttackEv = pokemon.AttackEv;
+            pokemonToUpdate.DefenseEv = pokemon.DefenseEv;
+            pokemonToUpdate.SpecialAttackEv = pokemon.SpecialAttackEv;
+            pokemonToUpdate.SpecialDefenseEv = pokemon.SpecialDefenseEv;
+            pokemonToUpdate.SpeedEv = pokemon.SpeedEv;
+            pokemonToUpdate.HealthIv = pokemon.HealthIv;
+            pokemonToUpdate.AttackIv = pokemon.AttackIv;
+            pokemonToUpdate.DefenseIv = pokemon.DefenseIv;
+            pokemonToUpdate.SpecialAttackIv = pokemon.SpecialAttackIv;
+            pokemonToUpdate.SpecialDefenseIv = pokemon.SpecialDefenseIv;
+            pokemonToUpdate.SpeedIv = pokemon.SpeedIv;
+            pokemonToUpdate.AbilityId = pokemon.AbilityId;
+            pokemonToUpdate.ItemId = pokemon.ItemId;
+            pokemonToUpdate.MoveOneId = pokemon.MoveOneId;
+            pokemonToUpdate.MoveTwoId = pokemon.MoveTwoId;
+            pokemonToUpdate.MoveThreeId = pokemon.MoveThreeId;
+            pokemonToUpdate.MoveFourId = pokemon.MoveFourId;
+            pokemonToUpdate.Level = pokemon.Level;
+            pokemonToUpdate.Experience = pokemon.Experience;
+            pokemonToUpdate.NatureId = pokemon.NatureId;
+            pokemonToUpdate.Gender = pokemon.Gender;
+            pokemonToUpdate.IsShiny = pokemon.IsShiny;
+
+            _dataContext.SaveChanges();
+
+            var pokemonGet = new PokemonGetDto
+            {
+                Id = id,
+                Name = pokemon.Name,
+                PokemonSpeciesId = pokemon.PokemonSpeciesId,
+                HealthEv = pokemon.HealthEv,
+                AttackEv = pokemon.AttackEv,
+                DefenseEv = pokemon.DefenseEv,
+                SpecialAttackEv = pokemon.SpecialAttackEv,
+                SpecialDefenseEv = pokemon.SpecialDefenseEv,
+                SpeedEv = pokemon.SpeedEv,
+                HealthIv = pokemon.HealthIv,
+                AttackIv = pokemon.AttackIv,
+                DefenseIv = pokemon.DefenseIv,
+                SpecialAttackIv = pokemon.SpecialAttackIv,
+                SpecialDefenseIv = pokemon.SpecialDefenseIv,
+                SpeedIv = pokemon.SpeedIv,
+                AbilityId = pokemon.AbilityId,
+                ItemId = pokemon.ItemId,
+                MoveOneId = pokemon.MoveOneId,
+                MoveTwoId = pokemon.MoveTwoId,
+                MoveThreeId = pokemon.MoveThreeId,
+                MoveFourId = pokemon.MoveFourId,
+                Level = pokemon.Level,
+                Experience = pokemon.Experience,
+                NatureId = pokemon.NatureId,
+                Gender = pokemon.Gender,
+                IsShiny = pokemon.IsShiny
+            };
+
+            response.Data = pokemonGet;
+
+            return Ok(response);
+        }
+        
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var response = new Response();
+
+            var pokemon = _dataContext.Pokemon.FirstOrDefault(x => x.Id == id);
+
+            if (pokemon == null)
+            {
+                response.AddError("id", "Pokemon not found.");
+                return NotFound(response);
+            }
+
+            _dataContext.Pokemon.Remove(pokemon);
+            _dataContext.SaveChanges();
+
+            return Ok(response);
         }
     }
 }
