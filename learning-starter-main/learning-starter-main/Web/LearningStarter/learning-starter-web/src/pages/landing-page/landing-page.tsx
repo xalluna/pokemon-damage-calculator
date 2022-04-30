bd9029afeeb65badb956670b629ebe88365fb1f7
Alluna <bbarr516@gmail.com>
2022-04-30 00:08:56 -0500

    -feat roll modal

--- learning-starter-main/learning-starter-main/Web/LearningStarter/learning-starter-web/src/pages/landing-page/landing-page.tsx
+++ learning-starter-main/learning-starter-main/Web/LearningStarter/learning-starter-web/src/pages/landing-page/landing-page.tsx
@@ -1,13 +1,55 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Button, Modal } from "semantic-ui-react";
 import "./landing-page.css";
+import { useState } from "react";
+import { routes } from "../../routes/config";
 
-//This is a basic Component, and since it is used inside of
-//'../../routes/config.tsx' line 31, that also makes it a page
 export const LandingPage = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="home-page-container">
-      <Header>Home Page</Header>
+    <div>
+      <div className="home-page-container">
+        <Header size="large">Home Page</Header>
+      </div>
+      <div className="home-button">
+        <Modal
+          className="modal"
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+          open={open}
+          closeIcon
+          dimmer="blurring"
+          trigger={
+            <Button color="red" size="massive">
+              DO NOT CLICK
+            </Button>
+          }
+        >
+          <div className="modal-content">
+            <div>
+              <Header size="huge" textAlign="center">
+                You are about to leave the site. Are you sure you want to
+                proceed?
+              </Header>
+              <div className="buttons">
+                <Button
+                  color="grey"
+                  size="huge"
+                  onClick={(open) => setOpen(false)}
+                >
+                  No
+                </Button>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                  <Button color="teal" size="huge">
+                    Yes
+                  </Button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 };
