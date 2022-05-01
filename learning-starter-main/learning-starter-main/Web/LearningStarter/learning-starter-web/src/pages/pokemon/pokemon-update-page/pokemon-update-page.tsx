e93d8908e77bbf254ad0f8fe91bc60b7eab17827
Alluna <bbarr516@gmail.com>
2022-05-01 13:54:26 -0500

    -feat dropdowns on pokemon update page

--- learning-starter-main/learning-starter-main/Web/LearningStarter/learning-starter-web/src/pages/pokemon/pokemon-update-page/pokemon-update-page.tsx
+++ learning-starter-main/learning-starter-main/Web/LearningStarter/learning-starter-web/src/pages/pokemon/pokemon-update-page/pokemon-update-page.tsx
@@ -1,10 +1,11 @@
 import { Formik, Form, Field } from "formik";
-import { Button, Input } from "semantic-ui-react";
+import { Button, Card, Dropdown, Header, Input } from "semantic-ui-react";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
   ApiResponse,
   PokemonGetDto,
+  PokemonOptionsDto,
   PokemonUpdateDto,
 } from "../../../constants/types";
 import { baseUrl } from "../../../constants/env-vars";
@@ -16,6 +17,7 @@ export const PokemonUpdatePage = () => {
   let match = useRouteMatch<{ id: string }>();
   const id = match.params.id;
   const [pokemon, setPokemon] = useState<PokemonGetDto>();
+  const [options, setOptions] = useState<PokemonOptionsDto>();
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -33,6 +35,22 @@ export const PokemonUpdatePage = () => {
       setPokemon(response.data.data);
     };
 
+    const fetchPokemonOptions = async () => {
+      const response = await axios.get<ApiResponse<PokemonOptionsDto>>(
+        `/api/pokemon/options`
+      );
+
+      if (response.data.hasErrors) {
+        response.data.errors.forEach((err) => {
+          console.log(err);
+          return;
+        });
+      }
+
+      setOptions(response.data.data);
+    };
+
+    fetchPokemonOptions();
     fetchPokemon();
   }, [id]);
 
@@ -81,159 +99,320 @@ export const PokemonUpdatePage = () => {
 
   return (
     <>
-      {pokemon && (
-        <Formik initialValues={pokemon} onSubmit={onSubmit}>
-          <Form id="form">
-            <div>
+      {pokemon && options && (
+        <Card>
+          <Header textAlign="center">Update Pokemon</Header>
+          <Formik initialValues={pokemon} onSubmit={onSubmit}>
+            <Form id="form">
               <div>
-                <label>Name</label>
-                <Field id="name" name="name">
-                  {({ field }) => <Input {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Name</label>
+                  <Field id="name" name="name" placeholder="Name">
+                    {({ field }) => <Input {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Species</label>
-                <Field id="pokemonSpeciesId" name="pokemonSpeciesId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Pokemon</label>
+                  <Field id="pokemonSpeciesId" name="pokemonSpeciesId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Pokemon"
+                        options={options.species}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Ability</label>
-                <Field id="abilityId" name="abilityId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Ability</label>
+                  <Field id="abilityId" name="abilityId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Ability"
+                        options={options.abilities}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Item</label>
-                <Field id="itemId" name="itemId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Item</label>
+                  <Field id="itemId" name="itemId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Item"
+                        options={options.items}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Level</label>
-                <Field id="level" name="level">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Level</label>
+                  <Field id="level" name="level">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Move 1</label>
-                <Field id="moveOneId" name="moveOneId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>Move 2</label>
-                <Field id="moveTwoId" name="moveTwoId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Move 1</label>
+                  <Field id="moveOneId" name="moveOneId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Move"
+                        options={options.moves}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
+                <div>
+                  <label>Move 2</label>
+                  <Field id="moveTwoId" name="moveTwoId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Move"
+                        options={options.moves}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Move 3</label>
-                <Field id="moveThreeId" name="moveThreeId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>Move 4</label>
-                <Field id="moveFourId" name="moveFourId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Move 3</label>
+                  <Field id="moveThreeId" name="moveThreeId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Move"
+                        options={options.moves}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
+                <div>
+                  <label>Move 4</label>
+                  <Field id="moveFourId" name="moveFourId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Move"
+                        options={options.moves}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>HP EVs</label>
-                <Field id="healthEv" name="healthEv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>HP IVs</label>
-                <Field id="healthIv" name="healthIv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>HP EVs</label>
+                  <Field id="healthEv" name="healthEv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
+                <div>
+                  <label>HP IVs</label>
+                  <Field id="healthIv" name="healthIv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Atk EVs</label>
-                <Field id="attackEv" name="attackEv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>Atk IVs</label>
-                <Field id="attackIv" name="attackIv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Atk EVs</label>
+                  <Field id="attackEv" name="attackEv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
+                <div>
+                  <label>Atk IVs</label>
+                  <Field id="attackIv" name="attackIv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Def EVs</label>
-                <Field id="defenseEv" name="defenseEv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>Def IVs</label>
-                <Field id="defenseIv" name="defenseIv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Def EVs</label>
+                  <Field id="defenseEv" name="defenseEv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
+                <div>
+                  <label>Def IVs</label>
+                  <Field id="defenseIv" name="defenseIv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>SpA EVs</label>
-                <Field id="specialAttackEv" name="specialAttackEv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>SpA IVs</label>
-                <Field id="specialAttackIv" name="specialAttackIv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>SpA EVs</label>
+                  <Field id="specialAttackEv" name="specialAttackEv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
+                <div>
+                  <label>SpA IVs</label>
+                  <Field id="specialAttackIv" name="specialAttackIv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>SpD EVs</label>
-                <Field id="specialDefenseEv" name="specialDefenseEv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>SpD IVs</label>
-                <Field id="specialDefenseIv" name="specialDefenseIv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>SpD EVs</label>
+                  <Field id="specialDefenseEv" name="specialDefenseEv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
+                <div>
+                  <label>SpD IVs</label>
+                  <Field id="specialDefenseIv" name="specialDefenseIv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Spe EVs</label>
-                <Field id="speedEv" name="speedEv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-                <label>Spe IVs</label>
-                <Field id="speedIv" name="speedIv">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Spe EVs</label>
+                  <Field id="speedEv" name="speedEv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
+                <div>
+                  <label>Spe IVs</label>
+                  <Field id="speedIv" name="speedIv">
+                    {({ field }) => <Input type="number" {...field} />}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Nature</label>
-                <Field id="natureId" name="natureId">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Nature</label>
+                  <Field id="natureId" name="natureId">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Nature"
+                        options={options.natures}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Gender</label>
-                <Field id="gender" name="gender">
-                  {({ field }) => <Input type="number" {...field} />}
-                </Field>
-              </div>
+                <div>
+                  <label>Gender</label>
+                  <Field id="gender" name="gender">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        placeholder="Gender"
+                        options={[
+                          {
+                            key: "Male",
+                            text: "Male",
+                            value: 0,
+                          },
+                          {
+                            key: "Female",
+                            text: "Female",
+                            value: 1,
+                          },
+                          {
+                            key: "Undefined",
+                            text: "Undefined",
+                            value: 2,
+                          },
+                        ]}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
 
-              <div>
-                <label>Shiny</label>
-                <Field id="isShiny" name="isShiny">
-                  {({ field }) => <Input {...field} />}
-                </Field>
+                <div>
+                  <label>Shiny</label>
+                  <Field id="isShiny" name="isShiny">
+                    {({ field, form }) => (
+                      <Dropdown
+                        {...field}
+                        selection
+                        clearable
+                        options={[
+                          {
+                            key: "Yes",
+                            text: "Yes",
+                            value: true,
+                          },
+                          {
+                            key: "No",
+                            text: "No",
+                            value: false,
+                          },
+                        ]}
+                        onChange={(_, { value }) => {
+                          form.setFieldValue(field.name, value);
+                        }}
+                      />
+                    )}
+                  </Field>
+                </div>
               </div>
-              <div>
-                <Button type="submit">Submit</Button>
-              </div>
-            </div>
-          </Form>
-        </Formik>
+            </Form>
+          </Formik>
+          <div>
+            <Button
+              content="Update"
+              color="teal"
+              size="large"
+              type="submit"
+              form="form"
+            />
+          </div>
+        </Card>
       )}
     </>
   );
